5eb751ae71541dd134c9de2478a1bfc99b044e9d
pedroemanuel42 <99288851+pedroemanuel42@users.noreply.github.com>
2022-02-23 15:01:19 -0300

    aula 13 e 14

---
+++ aula13/ambiente.js
@@ -0,0 +1,26 @@
+// estrutura de repetição com teste lógico no inicio
+
+var c = 1;
+while (c <= 6) { 
+    console.log(`Passo ${c}`);
+    c++; // c = c + 1
+};
+
+
+// estrutura de repetição com teste lógico no final "do {}"
+
+var c = 1;
+do {
+    console.log(`Passo ${c}`);
+    c++;
+} while ( c <= 6);
+
+// Fazendo com força bruta
+console.log('Tudo bem?');
+console.log('Tudo bem?');
+console.log('Tudo bem?');
+console.log('Tudo bem?');
+console.log('Tudo bem?');
+console.log('Tudo bem?');
+
+
---
+++ aula14/ambiente.js
@@ -0,0 +1,6 @@
+// estrutura de repetição com variável de controle
+console.log('Vai começar...')
+for (var c = 1;c <= 5;c++) {
+    console.log(`Passo ${c}`);
+};
+console.log('Fim!');
